d094f9ed3f3a12a775f0f584b135e57a45330d45
zachYoung01 <54455604+zachYoung01@users.noreply.github.com>
2020-03-02 17:30:54 -0700

    Update Node.cpp

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,19 +1,50 @@
-#include <iostream>
-#include "Node.h"
-#include "UUID.h"
+#include "node.h"
 
-using namespace std;
-
-
-Node::Node() {
-    // Create UUID
-    this->UUID = new_uuid();
-}
 ///creates the key space for the Node
 int Node::createKeySpace() {
 
 }
 
-void Node::printUUID() {
-    cout << this->UUID << endl;
+/**
+ * computes the generation rate of a node and all its peers.
+ *TODO alter funct so that it excludes the destination node in the computation
+ */
+double Node::computeAggregateGenRate() {
+    double totalPeerRate;
+
+    for (int i = 0; i < peers.size(); i++) {
+        totalPeerRate += peers.at(i).keyGenRate;
+    }
+
+    totalPeerRate = totalPeerRate * 0.30;
+
+    aggregateGenRate = totalPeerRate + keyGenRate;
+}
+/**
+ * TODO write next two functions
+ * I'm not certain of how to write these.
+ */
+double computeShortTermAllocationRatio(){
+
+}
+
+double computeLongTermAllocationRatio(){
+
+}
+
+/**
+ * TODO write function
+ * Requires understanding of the two above for computation
+ */
+double computeAggregateAllocationRatio() {
+
+}
+
+/**
+ * TODO write function
+ * found by dividing the relevant creation (gen) rate by the relevant allocation rate.
+ * How do I determine which rates are the relevant ones?
+ */
+double computeProvisioningRatio() {
+
 }
