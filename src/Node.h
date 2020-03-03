d9db7e62d6bdaea0f4a33f4024625b1569a7cc68
zachYoung01 <54455604+zachYoung01@users.noreply.github.com>
2020-03-02 17:29:45 -0700

    Update Node.h

--- src/Node.h
+++ src/Node.h
@@ -2,45 +2,61 @@
 #define LIGHTSYSTEMP_NODE_H
 
 #include <iostream>
-#include <list>
+#include <vector>
 
 ///TODO integrate RPC into the class
 ///TODO likely other things that I didn't consider.
 class Node {
-private:
-    std::string UUID;
-    int keySpace;
-    int keyGenRate;
-    int keyShareRate;
-    bool active;
-    std::list<Node> peers; ///called directConnection on the board.
-
 public:
     Node();
 
+    ///getter and setter for nodeID
+    ///Alter these based on the fact that the nodeID should be be a UUID
+    int getNodeID() const { return nodeID; }
+    void setNodeID(int nid) { nodeID = nid; }
+
+    ///getter and setter for keySpace
+    int getKeySpace() const { return keySpace; }
+    void setKeySpace(int ks) { keySpace = ks; }
+
+    ///getter and setter for keyGenRate
+    double getKeyGenRate() const { return keyGenRate; }
+    void setKeyGenRate(double kgr) { keyGenRate = kgr; }
+
     /**
-     * Creates the keyspace for the Node
+     *
      * @return
      */
-    int createKeySpace();
-
-    // Getters
-    std::string getUUID() const { return UUID; }
-    int getKeySpace() const { return keySpace; }
-    int getKeyGenRate() const { return keyGenRate; }
     int getKeyShareRate() const { return keyShareRate; }
-    bool getActive() const { return active; }
-
-    // Setters
-    void setNodeID(std::string newUUID) { UUID = newUUID; }
-    void setKeySpace(int ks) { keySpace = ks; }
-    void setKeyGenRate(int kgr) { keyGenRate = kgr; }
     void setKeyShareRate(int ksr) { keyShareRate = ksr; }
+
+    /**
+     *
+     * @return
+     */
+    bool getActive() const { return active; }
     void setActive(bool a) { active = a; }
 
+    int createKeySpace();
+    double computeAggregateGenRate();
+    double computeShortTermAllocationRatio();
+    double computeLongTermAllocationRatio();
+    double computeAggregateAllocationRatio();
+    double computeProvisioningRatio();
 
-    // Printing
-    void printUUID();
+
+private:
+    int nodeID; ///nodeID is a 128 bit UUID so this should presumably not be an int
+    int keySpace;
+    int keyShareRate;
+    double keyGenRate;
+    double aggregateGenRate;
+    double shortTermAllocationRatio;
+    double longTermAllocationRatio;
+    double aggregateAllocationRatio;
+    double provisioningRatio;
+    bool active;
+    std::vector<Node> peers; ///called directConnection on the board.
 };
 
 
