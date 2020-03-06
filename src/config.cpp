f2d3646ad2bea262d472f796b57b3524125c240b
Zach-Young010 <54455604+Zach-Young010@users.noreply.github.com>
2020-03-05 18:24:01 -0700

    Keeping JSON in main

--- src/config.cpp
+++ src/config.cpp
@@ -1,11 +1,25 @@
 #include <fstream>
 #include <string>
-
 #include "config.hpp"
+#include "json.hpp"
+using json = nlohmann::json;
 
-Config Config::fromJSONFile(std::ifstream jsonFile) {}
+Config Config::fromJSONFile(std::ifstream jsonFile) {
+    json jf = json::parse(jsonFile);
+    jf.at("AMOUNT_OF_NODES").get_to(jsonFile.AMOUNT_OF_NODES);
+    jf.at("fullyConnected").get_to(jsonFile.fullyConnected);
+    jf.at("csvOutPutValue").get_to(jsonFile.csvOutPutValue);
+    jf.at("creationRate").get_to(jsonFile.creationRate);
+    jf.at("networkScale").get_to(jsonFile.networkScale);
+}
 
-Config Config::fromJSONString(std::string json) {}
+Config Config::fromJSONString(std::string json) {
+    std::ifstream ifs;
+    ifs.open("test.json");
+
+
+
+}
 
 //void from_json(const json& j, gop& g) {
 //    j.at("SentenceStressScore").get_to(g.SentenceStressScore);
