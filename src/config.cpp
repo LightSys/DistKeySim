a96889838fea4ea02292a30f034093b16bf35960
Zach-Young010 <54455604+Zach-Young010@users.noreply.github.com>
2020-03-05 20:39:10 -0700

    Keeping JSON in main

--- src/config.cpp
+++ src/config.cpp
@@ -12,17 +12,32 @@ Config::Config(std::ifstream jsonFile) {
         if (jf.contains("numNodes")) {
             jf.at("numNodes").get_to(this->numNodes);
         }
+        else {
+            numNodes = DEFAULT_NUM_NODES;
+        }
         if (jf.contains("connectionMode")){
             jf.at("connectionMode").get_to(this->connectionMode);
         }
-        if(jf.contains("csvOutputPath")){
+        else {
+            connectionMode = DEFAULT_CSV_OUTPUT_PATH;
+        }
+        if(jf.contains("csvOutputPath")) {
             jf.at("csvOutputPath").get_to(this->csvOutputPath);
+        }
+        else{
+            csvOutputPath = DEFAULT_CSV_OUTPUT_PATH;
         }
         if(jf.contains("creationRate")){
             jf.at("creationRate").get_to(this->creationRate);
         }
+        else{
+            creationRate = DEFAULT_CREATION_RATE;
+        }
         if(jf.contains("networkScale")){
             jf.at("networkScale").get_to(this->networkScale);
+        }
+        else {
+            networkScale = DEFAULT_NETWORK_SCALE;
         }
     }
 }
