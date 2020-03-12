781c685469a9cc3436a1b479d406938fa0c1af7f
stratx21 <stratx21@penguin>
2020-03-12 16:47:16 -0600

    put the simulation run file in the simulations folder

--- client/GUI.py
+++ client/GUI.py
@@ -38,7 +38,7 @@ def setJsonVar(str_results_dict,hardknobs_results_dict):
     print("finished writing!")
     print("running script for simulation...")
     
-    subprocess.call("./run.sh "+str(times_run) + " " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3] + " " + sys.argv[4] + " > simulation_run_"+str(times_run)+".txt",shell=True)
+    subprocess.call("./run.sh "+str(times_run) + " " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3] + " " + sys.argv[4] + " > simulation_"+str(times_run)+"/simulation_run_"+str(times_run)+".txt",shell=True)
     
     print("finished running simulation script!")
     
