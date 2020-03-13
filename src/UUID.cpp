f1707f9202f0db2b644767fbb735f19d70eb12f4
etdickey <etdickey@gmail.com>
2020-03-12 22:49:46 -0500

    Bug fixing.

--- src/UUID.cpp
+++ src/UUID.cpp
@@ -17,6 +17,7 @@ HexDigest new_uuid() {
     for(int i = 0; i < TRUNCATED_UUID; i++){
         strUUID += sha_uuid[i];
     }
+    cout << "Returning: " << UUIDToHex(strUUID) << endl;
     return UUIDToHex(strUUID);
 }
 
