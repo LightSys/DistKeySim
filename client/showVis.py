421398baa263d7b8647351785bebc9b7456014d2
Terry Westley <twestley@sabonrai.com>
2021-02-23 12:46:45 -0500

    Add choice of DataVis 1 or 2

--- client/showVis.py
+++ client/showVis.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python3
 
-#
-# Usage: runVisOnly -g graphIsLog (True|False) -s statslog.csv
-#
+# Help: showVis -h
+
 import argparse
+import DataVis as datavis
 import DataVis2 as datavis2
 
 def parseCommandLineArgs():
     parser = argparse.ArgumentParser(
         description="Show Distributed Key Simulation visualization in browser")
     
-    parser.add_argument("graphIsLog", help="Graph axis is logrithmic")
-    
-    parser.add_argument("statsLog", default="statslog.csv",
-                        help="statslog.csv file path")
+    parser.add_argument("visNum", help="Visualization (1|2)", type=int)
+    parser.add_argument("graphIsLog", help="Graph axis is logarithmic (True|False)")
+    parser.add_argument("statsLog", help="statslog.csv file path")
     
     return parser.parse_args()
 
 if __name__ == "__main__":
 
     args = parseCommandLineArgs()
+    print(args.visNum, args.graphIsLog, args.statsLog)
 
-    if(args.graphIsLog):
-        datavis2.runDataAnalyticsLog(args.statsLog)
+    if args.graphIsLog.upper().startswith('T'):
+        if args.visNum == 1:
+            print("ERROR: Visualization 1 does not support logarithmic")
+        elif args.visNum == 2:
+            datavis2.runDataAnalyticsLog(args.statsLog)
     else:
-        datavis2.runDataAnalytics(args.statsLog)
+        if args.visNum == 1:
+            datavis.runDataAnalytics(args.statsLog)
+        elif args.visNum == 2:
+            datavis2.runDataAnalytics(args.statsLog)
