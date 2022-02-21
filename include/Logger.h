a829d791ec583b1d2a36837b80da607eb60e0a67
Terry Westley <github@sabonrai.com>
2022-02-21 10:23:31 -0500

    Log messages

--- include/Logger.h
+++ include/Logger.h
@@ -7,10 +7,13 @@
 #include <chrono>
 #include <ctime>
 #include <vector>
+#include "message.hpp"
 using namespace std;
 
 const char filename[] = "logOutput.txt";  // general log file
 const char statslog[] = "statslog.csv";   // log file for stats output
+const char infoMsg[] = "infoMsg.txt";  // Info Msg log file
+const char keyMsg[] = "keyMsg.txt";  // Keyspace Msg log file
 
 const vector<string> csvHeaders {
     "UUID", "timeSlot", "totalKeys","sharing", "consumption", "remainder", "numKeyspaces"
@@ -21,6 +24,7 @@ class Logger {
         static void deleteOldLog ();
         static void log (string message);
         static void logStats (vector<string> stats);
+        static void logMsg (const string procName, const Message &message);
 
         static void setCSVHeaders();
         static int getTimeslot(bool increment);
@@ -29,6 +33,14 @@ class Logger {
     	static std::string copyFile(string path);
 
         static std::string join(vector<int> ints);
+        static std::string join(vector<string> stats, string separator);
+
+        static void logBackTrace();
+
+    private:
+        static int timeslot;
+        static int shared;
+        static int rate;
 };
 
 // From https://stackoverflow.com/questions/5396790/concatenate-strings-in-function-call-c
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,13 +1,23 @@
 #include <numeric>
+#include <regex>
+#include <libunwind.h>
+
+#include <google/protobuf/text_format.h>
 
 #include "Logger.h"
 
 static ofstream logOutputStream;
 static ofstream logStatsStream;
 
+int Logger::timeslot = 0;
+int Logger::shared = 0;
+int Logger::rate = 0;
+
 void Logger::deleteOldLog() {
     remove(filename);
     remove(statslog);
+    remove(infoMsg);
+    remove(keyMsg);
     logOutputStream.open(filename, ofstream::app);
     logStatsStream.open(statslog, ofstream::app);
 }
@@ -20,13 +30,22 @@ void Logger::log(string message) {
 
 void Logger::logStats(vector<string> stats) {
     if (stats.size() == csvHeaders.size()) {
-        for (int i = 0; i < stats.size(); i++) {  // write each line to csv
-            logStatsStream << stats[i] << ",";
-        }
+        logStatsStream << join(stats, ",");
         logStatsStream << "\n";
     } else {  // don't log if array isn't accurate size
         log("Stats array was incorrect length");
     }
+}
+
+void Logger::logMsg (const string procName, const Message &message) {
+    std::string prototextOutput;
+    google::protobuf::TextFormat::PrintToString(message, &prototextOutput);
+    // Logger::log(Formatter() << procName << ": " << prototextOutput);
+
+    // Log the message as JSON on one line (this is not real JSON, but close enough)
+    auto removeNewLine = std::regex_replace(prototextOutput, std::regex("([^{])\\n([^}])"), "$1, $2");
+    auto squeezeWhitespace = std::regex_replace(removeNewLine, std::regex("\\s+"), " ");
+    Logger::log(Formatter() << procName << ": {" << squeezeWhitespace << "}");
 }
 
 void Logger::setCSVHeaders() {
@@ -34,7 +53,6 @@ void Logger::setCSVHeaders() {
 }
 
 int Logger::getTimeslot(bool increment) {
-    int timeslot = 0;
     if (increment) {  // only increment if it is from doAllHearbeat()
         timeslot++;
     }
@@ -42,7 +60,6 @@ int Logger::getTimeslot(bool increment) {
 }
 
 int Logger::getShared(bool clear, int more) {
-    int shared = 0;
     shared += more;
     if (clear) {  // reset after timeslot changes
         shared = 0;
@@ -51,7 +68,6 @@ int Logger::getShared(bool clear, int more) {
 }
 
 int Logger::getConsumption(bool clear, int more) {
-    int rate = 0;
     rate += more;
     if (clear) {  // reset after timeslot changes
         rate = 0;
@@ -121,4 +137,45 @@ std::string Logger::join(vector<int> ints) {
         [](const std::string& a, int b){
             return a + ',' + std::to_string(b);
         });
+}
+
+std::string Logger::join(vector<string> strings, string separator) {
+    if (strings.empty()) {
+        return std::string();
+    }
+    return std::accumulate(strings.begin()+1, strings.end(), strings[0],
+        [separator](const std::string& a, string b){
+            return a + separator + b;
+        });
+}
+
+void Logger::logBackTrace() {
+  unw_cursor_t cursor;
+  unw_context_t context;
+
+  // Initialize cursor to current frame for local unwinding.
+  unw_getcontext(&context);
+  unw_init_local(&cursor, &context);
+
+  // Unwind frames one by one, going up the frame stack.
+  while (unw_step(&cursor) > 0) {
+    unw_word_t offset, pc;
+    unw_get_reg(&cursor, UNW_REG_IP, &pc);
+    if (pc == 0) {
+      break;
+    }
+    // printf("0x%lx:", pc);
+    char str[1000];
+    // sprintf(str, "0x%lx:", pc);
+    // Logger::log(std::string(str));
+
+    char sym[256];
+    if (unw_get_proc_name(&cursor, sym, sizeof(sym), &offset) == 0) {
+    //   printf(" (%s+0x%lx)\n", sym, offset);
+      sprintf(str, " (%s+0x%lx)", sym, offset);
+      Logger::log(std::string(str));
+    } else {
+      Logger::log("ERROR: unable to obtain symbol name for this frame");
+    }
+  }
 }
